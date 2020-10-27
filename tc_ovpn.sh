02cec86de6f94116075affe74121b83f1f0acd22
adminprivileges <caldwellthaddeus@protonmail.com>
2020-10-27 07:21:16 -0400

    inital commit

---
+++ tc_ovpn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Updating Your Machine"
+apt update -y && apt upgrade -y > /dev/null
+echo "Finding your Public IP" 
+PUB_IP= `curl -q ifconfig.me`
+echo $PUB_IP > ./pub_ip.var
+echo "Your Public IP is: $PUB_IP  Please take note of this."
+curl https://git.io/vpn -o ./openvpn-install.sh
+chmod +x ./openvpn-install.sh
+./openvpn-install.sh
+sudo systemctl start openvpn-server@server
+sudo systemctl enable openvpn-server@server
+
