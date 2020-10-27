36b481c1013aed545324836c1575c5a8875006a2
adminprivileges <caldwellthaddeus@protonmail.com>
2020-10-27 07:33:08 -0400

    inital commit

--- tc_ovpn.sh
+++ tc_ovpn.sh
@@ -8,6 +8,7 @@ echo "Your Public IP is: $PUB_IP  Please take note of this."
 curl https://git.io/vpn -o ./openvpn-install.sh
 chmod +x ./openvpn-install.sh
 ./openvpn-install.sh
+#Starting and Enabling ovpn service 
 sudo systemctl start openvpn-server@server
 sudo systemctl enable openvpn-server@server
 
