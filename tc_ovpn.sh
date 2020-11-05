30752a92f181016d1f14120ffcef756caa6cb223
DOOM <caldwellthaddeus@protonmail.com>
2020-11-05 15:26:10 -0500

    Update tc_ovpn.sh

--- tc_ovpn.sh
+++ tc_ovpn.sh
@@ -4,7 +4,7 @@ apt update -y &> /dev/null && apt upgrade -y &> /dev/null
 echo "Finding your Public IP" 
 PUB_IP=`curl -q ifconfig.me &> /dev/null`
 echo $PUB_IP > ./pub_ip.var
-echo "Your Public IP is: $PUB_IP  Please take note of this."
+echo "Your Public IP is: $PUB_IP.  Please take note of this."
 curl https://git.io/vpn -o ./openvpn-install.sh &> /dev/null
 chmod +x ./openvpn-install.sh
 ./openvpn-install.sh
