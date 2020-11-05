95b7037f15bde0fbf1792d8f57e3f35f4b519b1a
DOOM <caldwellthaddeus@protonmail.com>
2020-11-05 15:39:04 -0500

    Update tc_ovpn.sh
    specifying bash for the openvpn install

--- tc_ovpn.sh
+++ tc_ovpn.sh
@@ -7,7 +7,7 @@ echo $PUB_IP > ./pub_ip.var
 echo "Your Public IP is: $PUB_IP.  Please take note of this."
 curl -sSL https://git.io/vpn -o ./openvpn-install.sh &> /dev/null
 chmod +x ./openvpn-install.sh
-./openvpn-install.sh
+bash ./openvpn-install.sh
 #Starting and Enabling ovpn service 
 sudo systemctl start openvpn-server@server
 sudo systemctl enable openvpn-server@server
